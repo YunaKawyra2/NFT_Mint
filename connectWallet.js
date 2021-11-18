8107adcf5247dff43c6eebcbb9fc9e17438e1b55
YunaKawyra2 <94621984+YunaKawyra2@users.noreply.github.com>
2021-11-18 22:31:11 +0100

    Add files via upload

--- connectWallet.js
+++ connectWallet.js
@@ -1,7 +1,11 @@
 /*export const web3 = new Web3(ethereum);*/
 /*export const web3 = Web3(Web3.HTTPProvider('https://ropsten.infura.io/v3/9912d6b0054b4a249bef9b00e28acef1'));*/
 
-web3 = new Web3(new Web3.providers.HttpProvider('https://ropsten.infura.io/v3/9912d6b0054b4a249bef9b00e28acef1'));
+/*web3 = new Web3(new Web3.providers.HttpProvider('https://ropsten.infura.io/v3/9912d6b0054b4a249bef9b00e28acef1'));*/
+export const Web3 = require("web3");
+web3 = new Web3(new Web3.providers.WebsocketProvider('wws://ropsten.infura.io/v3/9912d6b0054b4a249bef9b00e28acef1'));
+
+
 
 const isMetaMaskConnected = async () => {
     let accounts = await web3.eth.getAccounts();
