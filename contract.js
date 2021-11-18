03671b7679fd20023c6c359ee7f955d50367d23d
YunaKawyra2 <94621984+YunaKawyra2@users.noreply.github.com>
2021-11-18 16:09:59 +0100

    Add files via upload

--- contract.js
+++ contract.js
@@ -6,13 +6,16 @@ export let address;
 export let abi;
 export let mintPrice;
 
-if (window?.WEBSITE_URL?.includes(window.location.hostname)) {
+/*if (window?.WEBSITE_URL?.includes(window.location.hostname)) {
   address = window.CONTRACT_ADDRESS;
   abi = typeof window.CONTRACT_ABI === 'string'
       ? JSON.parse(window.CONTRACT_ABI) : window.CONTRACT_ABI;
 }else {
   address = TestIceCreamAddress;
   abi = TestIceCreamAbi;
-} 
+} */
+
+address = TestIceCreamAddress;
+abi = TestIceCreamAbi;
 
 export let contract = new web3.eth.Contract(abi, address);
