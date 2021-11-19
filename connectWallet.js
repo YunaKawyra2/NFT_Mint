a6de24df120bdb2f2b1402b6ce14f0fb37192e4d
YunaKawyra2 <94621984+YunaKawyra2@users.noreply.github.com>
2021-11-19 10:06:09 +0100

    Add files via upload

--- connectWallet.js
+++ connectWallet.js
@@ -1,8 +1,8 @@
-/*export const web3 = new Web3(ethereum);*/
+export const web3 = new Web3(ethereum);
 /*export const web3 = Web3(Web3.HTTPProvider('https://ropsten.infura.io/v3/9912d6b0054b4a249bef9b00e28acef1'));*/
 
 /*web3 = new Web3(new Web3.providers.HttpProvider('https://ropsten.infura.io/v3/9912d6b0054b4a249bef9b00e28acef1'));*/
-export const web3 = new Web3(new Web3.providers.WebsocketProvider('wws://ropsten.infura.io/v3/9912d6b0054b4a249bef9b00e28acef1'));
+/*export const web3 = new Web3(new Web3.providers.WebsocketProvider('wws://ropsten.infura.io/v3/9912d6b0054b4a249bef9b00e28acef1'));*/
 
 
 
